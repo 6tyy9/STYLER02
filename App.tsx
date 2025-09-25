3bfcda50ef66d31f7b50a4043b22f4a0803aa628
6tyy9 <rawat.shivanshu007@gmail.com>
2025-09-25 15:20:22 +0530

    worked with scrolls

--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import FlatCards from './components/FlatCards'
+import ElevatedCards from './components/ElevatedCards'
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>App</Text>
         <FlatCards/>
+        <ElevatedCards/>
       </ScrollView>
     </SafeAreaView>
   )
---
+++ components/ElevatedCards.tsx
@@ -0,0 +1,56 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ElevatedCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>ElevatedCards</Text>
+      <ScrollView style={styles.container} horizontal={true}>
+        <View style={[styles.card, styles.cadElevated]}>
+            <Text>Tap</Text>
+        </View>
+        <View style={[styles.card, styles.cadElevated]}>
+            <Text>me</Text>
+        </View>
+        <View style={[styles.card, styles.cadElevated]}>
+            <Text>to</Text>
+        </View>
+        <View style={[styles.card, styles.cadElevated]}>
+            <Text>scroll</Text>
+        </View>
+        <View style={[styles.card, styles.cadElevated]}>
+            <Text>more...</Text>
+        </View>
+        <View style={[styles.card, styles.cadElevated]}>
+            <Text>🤡</Text>
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+    },
+    card:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        width:100,
+        height:100,
+        borderRadius:4,
+        margin:8,
+    },
+    cadElevated:{
+        backgroundColor:'lightblue',
+        elevation:4,
+        shadowOffset:{width:1, height:1},
+        shadowColor:'#333',
+    },
+    container:{
+        padding:8
+    }
+})
